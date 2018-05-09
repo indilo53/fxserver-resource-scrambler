f7117e97c41f08aba886f355145fbea456b96a11
Jérémie N'gadi <jeremie.ngadi@gmail.com>
2018-05-09 12:12:08 +0200

    Change node-lua version in package.json

--- resourcescrambler.js
+++ resourcescrambler.js
@@ -1,7 +1,7 @@
 const fs      = require('fs');
 const glob    = require('glob');
 const uuidv4  = require('uuid/v4');
-const nodelua = require('node-lua');
+const nodelua = require('nodelua');
 
 class ResourceScrambler {
 
